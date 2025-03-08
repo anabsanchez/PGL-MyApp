fea6cf9f45996ce79d65787c5a9b95f161267fff
anabsanchez <anabsanchez.dev@gmail.com>
2025-03-08 00:31:15 +0000

    feat: added theme changing icon to welcome page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,20 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
 import ThemeProvider from "../providers/ThemeProvider";
+import ThemeSwitch from "../components/ThemeSwitch";
 
 export default function AppLayout() {
   return (
     <ThemeProvider>
       <Stack>
+        <Stack.Screen
+          name="welcome"
+          options={{
+            headerTitle: "Home",
+            headerShown: true,
+            headerRight: () => <ThemeSwitch />,
+          }}
+        />
         <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
       </Stack>
     </ThemeProvider>
